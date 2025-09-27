b90063235bc2489eab08e3577098271cccc0af20
ada6967 <aadya.gari@gmail.com>
2025-09-27 16:28:43 +0100

    Add files via upload

---
+++ Nested_if.py
@@ -0,0 +1,16 @@
+Height=130
+Coins=5
+
+if Height >=120:
+  print("You can ride the rollercoaster and have fun unlike the shorties that have to go on the kiddie rides.")
+
+  if Coins >=10:
+    print("You have enough money to not wait at any lines and are not poor like the peasants you see lining up.")
+
+  else:
+    print("You are poor and have to wait for hours while the fastpass people flex on you.")
+
+else:
+  print("You cant ride the rollercoster because you are too short and have to ride on the kiddie rollercoster with your mummy and daddy.")
+
+  
