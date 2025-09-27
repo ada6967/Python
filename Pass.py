7aa4d9126d65b2baafd3063a73e9602b5dadcc63
ada6967 <aadya.gari@gmail.com>
2025-09-27 16:35:01 +0100

    Add files via upload

---
+++ Pass.py
@@ -0,0 +1,6 @@
+Child=False
+
+if Child == False:
+  pass 
+else:
+  print("You are a child and can enjoy the candy because you don't have diabetes and can flex on the with it but your going to get it once you eat the candy we give you.")
