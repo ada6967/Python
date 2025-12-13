f3291e194666d3d0b5e4b6001c993b85cce342dd
ada6967 <aadya.gari@gmail.com>
2025-12-13 16:54:22 +0000

    Add files via upload

---
+++ Lambda_Functions.py
@@ -0,0 +1,29 @@
+#Example 1:
+
+greeting= lambda: print("Greetings master!")
+
+greeting()
+
+#Example 2:
+
+fruit= lambda: print("My favourite fruit is grape")
+
+fruit()
+
+#Example 3:
+
+add= lambda a,b: print(a+b)
+
+add(99,777)
+
+#Example 4:
+
+multi= lambda c,d: print(c*d)
+
+multi(6,7)
+
+#Example 5:
+
+add_str= lambda e,f,g: print(f"{e} {f} {g}")
+
+add_str("hi", "hello", "hey")
