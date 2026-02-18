d95d0bf6cdc9a3f4b3882f247bcae3f7764a96a5
Ada xx <aadya.gari@gmail.com>
2026-02-18 15:17:37 +0000

    Add files via upload

---
+++ Functions/User_Define_Function.py
@@ -0,0 +1,34 @@
+#Example 1:
+
+def greeting():
+  print("Greetings master!")
+
+greeting()
+
+#Example 2:
+
+def fruit():
+  print("My favourite fruit is a grape")
+
+fruit()
+
+#Example 3:
+
+def add(a,b):
+  print(a+b)
+
+add(99,777)
+
+#Example 4:
+
+def multi(c,d):
+  e=c*d
+  print(e)
+
+multi(6,7)
+
+#Example 5:
+def add_str(f,g,h):
+  print(f"{f} {g} {h}")
+
+add_str("hi", "hello", "hey")
