2607dbdc16a4a1deb8a866a2471ca7ea3d35c809
Ada xx <aadya.gari@gmail.com>
2026-02-18 15:17:56 +0000

    Add files via upload

---
+++ Loops/While.py
@@ -0,0 +1,20 @@
+#Print numbers between 1 - 5
+
+i = 1
+while i <= 5:
+  print(i)
+  i = i+1
+
+#Print even numbers from 2 - 10
+
+i = 2
+while i <= 10:
+  print(i)
+  i = i+2
+
+#Count down from 5 - 0
+
+i = 5
+while i >=0:
+  print(i)
+  i = i-1
